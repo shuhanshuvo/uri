b2cffc3badbcba53de00a6e14c5784c2983cc3c8
Shuhanur Rahman <shuhanshuvo85@gmail.com>
2016-05-16 01:36:30 +0600

    Add files via upload

---
+++ 1005.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  float a,b,MEDIA;
+  scanf("%f",&a);
+  scanf("%f",&b);
+  MEDIA = (a*3.5+b*7.5)/(3.5+7.5);
+  printf("MEDIA = %0.5f\n",MEDIA);
+
+
+    return 0;
+
+
+}
