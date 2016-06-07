aca49d25d2f64d4dd6d6d92ab4628b6d95b9843e
Shuhanur Rahman <shuhanshuvo85@gmail.com>
2016-06-07 15:23:38 +0600

    Add files via upload

---
+++ 1043.c
@@ -0,0 +1,19 @@
+int main() {
+
+
+
+    float a, b, c;
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    if(a+b>c && a+c>b && b+c>a){
+
+        printf("Perimetro = %.1f\n", a+b+c);
+
+    }
+
+    else{printf("Area = %.1f\n", .5*(a+b)*c);}
+
+    return 0;
+
+}
