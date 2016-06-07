b1ec1b709b45ec34f8ec582cccf65b5e2be83fdb
Shuhanur Rahman <shuhanshuvo85@gmail.com>
2016-06-07 15:04:28 +0600

    Add files via upload

---
+++ 1020.c
@@ -0,0 +1,18 @@
+int main()
+
+{
+
+    int n,y,m,d;
+
+    scanf("%d",&n);
+
+    y=n/365;
+
+    m=(n%365)/30;
+
+    d=(n%365)%30;
+
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n",y,m,d);
+
+
+    return 0;}
